ba1b28258ca742b86295e3ff94aa50d531eb9bbe
Iuri Pereira <689440+iuricmp@users.noreply.github.com>
2024-01-09 23:48:23 +0000

    chore: hello gno

--- packages/mobile/app/index.tsx
+++ packages/mobile/app/index.tsx
@@ -1,7 +1,29 @@
 import { Button, StyleSheet, Text, View, Pressable } from "react-native";
 import { Link } from 'expo-router';
 
+// order matters here
+import "react-native-polyfill-globals/auto";
+
+// Polyfill async.Iterator. For some reason, the Babel presets and plugins are not doing the trick.
+// Code from here: https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-3.html#caveats
+(Symbol as any).asyncIterator =
+  Symbol.asyncIterator || Symbol.for("Symbol.asyncIterator");
+
+import { useGno } from "@gno/hooks/use-gno";
+import React from "react";
+
 export default function Page() {
+
+  const gno = useGno();
+
+  React.useEffect(() => {
+    gno
+      .render("gno.land/r/demo/boards", "gnonative/1")
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }, []);
+
+
   return (
     <View style={styles.container}>
       <View style={styles.main}>
