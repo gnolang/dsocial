5cfb30a243b26757b08b39cd22cbbbf380245823
Iuri Pereira <689440+iuricmp@users.noreply.github.com>
2024-01-09 22:24:41 +0000

    chroe: expo router setup

---
+++ packages/mobile/app/index.tsx
@@ -0,0 +1,43 @@
+import { Button, StyleSheet, Text, View, Pressable } from "react-native";
+import { Link } from 'expo-router';
+
+export default function Page() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.main}>
+        <Text style={styles.title}>Hello World</Text>
+        <Text style={styles.subtitle}>This is the first page of your app.</Text>
+      </View>
+
+
+      <Link href="/signup" asChild>
+      <Pressable>
+        <Text>Create account</Text>
+      </Pressable>
+      </Link>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 24,
+  },
+  main: {
+    flex: 1,
+    justifyContent: "center",
+    maxWidth: 960,
+    marginHorizontal: "auto",
+  },
+  title: {
+    fontSize: 64,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontSize: 36,
+    color: "#38434D",
+  },
+});
