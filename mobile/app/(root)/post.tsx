7348d269c00e89829180cf039c8d5f30d1663671
Iuri <689440+iuricmp@users.noreply.github.com>
2024-02-27 19:00:52 +0000

    chore: add JSON thread retrieval functionality (#21)
    * chore: add JSON thread retrieval functionality
    
    ---------
    
    Signed-off-by: Iuri Pereira <689440+iuricmp@users.noreply.github.com>

--- mobile/app/(root)/post.tsx
+++ mobile/app/(root)/post.tsx
@@ -19,6 +19,7 @@ export default function Page() {
 
   useEffect(() => {
     const unsubscribe = navigation.addListener("focus", async () => {
+      setPostContent("");
       try {
         const response = await gno.getActiveAccount();
         if (!response.key) throw new Error("No active account");
@@ -40,7 +41,8 @@ export default function Page() {
       for await (const response of await gno.call("gno.land/r/berty/social", "PostMessage", args, gasFee, gasWanted)) {
         console.log("response: ", response);
       }
-      router.push("/home");
+      setPostContent("");
+      router.push("home");
     } catch (error) {
       console.log(error);
       setError("" + error);
@@ -68,6 +70,7 @@ export default function Page() {
           <TextInput
             placeholder="What's happening?"
             onChangeText={setPostContent}
+            value={postContent}
             multiline
             numberOfLines={4}
             style={{ height: 100 }}
---
+++ mobile/src/hooks/use-feed.ts
@@ -0,0 +1,58 @@
+import { Post } from "@gno/types";
+import { useGno } from "./use-gno";
+
+export const useFeed = () => {
+  const gno = useGno();
+
+  async function fetchFeed(startIndex: number, endIndex: number): Promise<{ data: Post[]; n_threads: number }> {
+    const currentAccount = await gno.getActiveAccount();
+    if (!currentAccount.key) throw new Error("No active account");
+    const bech32 = await gno.addressToBech32(currentAccount.key.address);
+    const result = await gno.qEval("gno.land/r/berty/social", `GetThreadPosts("${bech32}" , 0, 0, ${startIndex}, ${endIndex})`);
+    const json = convertToPosts(result, currentAccount.key.name);
+    return json;
+  }
+
+  function convertToPosts(result: string | undefined, username: string) {
+    if (!result || !(result.startsWith("(") && result.endsWith(" string)"))) throw new Error("Malformed GetThreadPosts response");
+    const quoted = result.substring(1, result.length - " string)".length);
+    const json = JSON.parse(quoted);
+    const jsonPosts = JSON.parse(json);
+    console.log(jsonPosts);
+
+    if (!jsonPosts.n_threads || jsonPosts.n_threads === 0)
+      return {
+        data: [],
+        n_threads: 0,
+      };
+
+    const posts: Post[] = [];
+
+    jsonPosts.posts.forEach((post: any) => {
+      posts.push({
+        user: {
+          user: username,
+          name: username,
+          image: "https://www.gravatar.com/avatar/tmp",
+          followers: 0,
+          url: "string",
+          bio: "string",
+        },
+        index: post.index,
+        id: Math.random().toString(),
+        post: post.post.body,
+        date: post.post.createdAt,
+        n_replies: post.post.n_replies,
+        n_replies_all: post.post.n_replies_all,
+        parent_id: post.post.parent_id,
+      });
+    });
+
+    return {
+      data: posts,
+      n_threads: jsonPosts.n_threads,
+    };
+  }
+
+  return { fetchFeed };
+};
