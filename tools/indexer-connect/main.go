24e7d4a8c4ad43d077afc564dc2f1b4ce11881f7
Jeff Thompson <jeff@thefirst.org>
2024-04-10 08:54:49 +0200

    chore: in indexer-connect, set up gnonative client
    Signed-off-by: Jeff Thompson <jeff@thefirst.org>

--- tools/indexer-connect/main.go
+++ tools/indexer-connect/main.go
@@ -1,4 +1,39 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gnolang/gnonative/api/gen/go/_goconnect"
+	"github.com/gnolang/gnonative/service"
+)
+
 func main() {
+	// Start the Gno Native Kit gRPC service where the remote is gnoland.
+	options := []service.GnoNativeOption{
+		service.WithTcpAddr("localhost:0"),
+		service.WithUseTcpListener(),
+	}
+	service, err := service.NewGnoNativeService(options...)
+	if err != nil {
+		log.Fatalf("failed to run GnoNativeService: %v", err)
+		return
+	}
+	defer service.Close()
+
+	// Create a Gno Native Kit gRPC client.
+	client := _goconnect.NewGnoNativeServiceClient(
+		http.DefaultClient,
+		fmt.Sprintf("http://localhost:%d", service.GetTcpPort()),
+	)
+
+	if err := run(client); err != nil {
+		log.Fatal(err)
+		return
+	}
+}
+
+func run(client _goconnect.GnoNativeServiceClient) error {
+	return nil
 }
