6ecfb4afe1f957eca7d9e00a72c80212b934a666
D4ryl00 <d4ryl00@gmail.com>
2024-04-16 16:31:39 +0200

    feat: use WebSocket Genqlient
    Signed-off-by: D4ryl00 <d4ryl00@gmail.com>

--- tools/indexer-connect/main.go
+++ tools/indexer-connect/main.go
@@ -3,84 +3,106 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 	"net/http"
+	"time"
 
+	"github.com/Khan/genqlient/graphql"
 	"github.com/gnolang/gnonative/api/gen/go/_goconnect"
-	"github.com/gnolang/gnonative/service"
-	graphql "github.com/hasura/go-graphql-client"
+	"github.com/gorilla/websocket"
 )
 
-var remote = "http://testnet.gno.berty.io:8546/graphql/query"
+var remote = "testnet.gno.berty.io:8546/graphql/query"
 
 type IndexerClient struct {
 	gnoNativeClient _goconnect.GnoNativeServiceClient // Gno Native Kit gRPC client
-	iClient         graphql.Client
+	iClient         graphql.WebSocketClient
 }
 
 func main() {
 	// Start the Gno Native Kit gRPC service where the remote is gnoland.
-	options := []service.GnoNativeOption{
-		service.WithTcpAddr("localhost:0"),
-		service.WithUseTcpListener(),
-	}
-	service, err := service.NewGnoNativeService(options...)
-	if err != nil {
-		log.Fatalf("failed to run GnoNativeService: %v", err)
-		return
-	}
-	defer service.Close()
+	// options := []service.GnoNativeOption{
+	// 	service.WithTcpAddr("localhost:0"),
+	// 	service.WithUseTcpListener(),
+	// }
+	// service, err := service.NewGnoNativeService(options...)
+	// if err != nil {
+	// 	log.Fatalf("failed to run GnoNativeService: %v", err)
+	// 	return
+	// }
+	// defer service.Close()
 
 	// Create a Gno Native Kit gRPC client.
-	gnoNativeClient := _goconnect.NewGnoNativeServiceClient(
-		http.DefaultClient,
-		fmt.Sprintf("http://localhost:%d", service.GetTcpPort()),
-	)
-
-	if err := run(gnoNativeClient); err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	// var filter struct {
-	// 	Filter struct {
-	// 		Message struct {
-	// 			VmParam struct {
-	// 				Exec struct {
-	// 					Func    string `graphql:"func: \"Follow\""`
-	// 					PkgPath string `graphql:"pkg_path: \"gno.land/r/berty/social\""`
-	// 				} `graphql:"exec"`
-	// 			} `graphql:"vm_param"`
-	// 		} `graphql:"message"`
-	// 	} `graphql:"filter"`
+	// gnoNativeClient := _goconnect.NewGnoNativeServiceClient(
+	// 	http.DefaultClient,
+	// 	fmt.Sprintf("http://localhost:%d", service.GetTcpPort()),
+	// )
+	//
+	// if err := run(gnoNativeClient); err != nil {
+	// 	log.Fatal(err)
+	// 	return
 	// }
 
-	var transactions struct {
-		Transactions struct {
-			Hash string `graphql:"hash"`
-		} `graphql:"transactions(filter: $filter)"`
-	}
-
-	var filter struct {
-		Filter struct {
-		} `graphql:"filter"`
-	}
-
-	variables := map[string]interface{}{
-		"filter": filter,
-	}
+	ctx := context.Background()
 
 	// Create a new RPC client.
-	iClient := graphql.NewClient(remote, nil)
-	err = iClient.Query(context.Background(), &transactions, variables)
+	iClient := graphql.NewClient("http://"+remote, http.DefaultClient)
+	resp, err := getTransactions(ctx, iClient, "g1jg8mtutu9khhfwc4nxmuhcpftf0pajdhfvsqf5")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println(transactions)
+	fmt.Println(resp)
+
+	wsClient := graphql.NewClientUsingWebSocket(
+		"ws://"+remote,
+		&MyDialer{Dialer: websocket.DefaultDialer},
+		nil,
+	)
+
+	errChan, err := wsClient.StartWebSocket(ctx)
+	if err != nil {
+		return
+	}
+
+	dataChan, subscriptionID, err := subscribeBlocks(ctx, wsClient)
+	if err != nil {
+		return
+	}
+
+	defer wsClient.CloseWebSocket()
+	for loop := true; loop; {
+		select {
+		case msg, more := <-dataChan:
+			if !more {
+				loop = false
+				break
+			}
+			if msg.Data != nil {
+				fmt.Println(msg.Data.Blocks.Height)
+			}
+			if msg.Errors != nil {
+				fmt.Println("error:", msg.Errors)
+				loop = false
+			}
+		case err = <-errChan:
+			return
+		case <-time.After(time.Minute):
+			err = wsClient.Unsubscribe(subscriptionID)
+			loop = false
+		}
+	}
 
 }
 
 func run(client _goconnect.GnoNativeServiceClient) error {
 	return nil
+}
+
+type MyDialer struct {
+	*websocket.Dialer
+}
+
+func (md *MyDialer) DialContext(ctx context.Context, urlStr string, requestHeader http.Header) (graphql.WSConn, error) {
+	conn, _, err := md.Dialer.DialContext(ctx, urlStr, requestHeader)
+	return graphql.WSConn(conn), err
 }
