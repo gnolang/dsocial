5223b0930c409f66d62cb32cfd7c45b3ae75aac4
Iuri Pereira <689440+iuricmp@users.noreply.github.com>
2024-04-16 14:49:14 +0100

    fix: remove account button
    Signed-off-by: Iuri Pereira <689440+iuricmp@users.noreply.github.com>

---
+++ mobile/app/[account]/remove.tsx
@@ -0,0 +1,19 @@
+import { selectFollowers } from "redux/features/profileSlice";
+import { logedOut, useAppDispatch, useAppSelector } from "@gno/redux";
+import { User } from "@gno/types";
+import RemoveAccountContent from "@gno/components/view/account/remove-account.tsx";
+import { useGno } from "@gno/hooks/use-gno";
+
+export default function Page() {
+  const gno = useGno();
+  const dispatch = useAppDispatch();
+  const data = useAppSelector(selectFollowers);
+
+  const onConfirm = async (item: User) => {
+    await gno.deleteAccount(item.address, undefined, true);
+
+    dispatch(logedOut());
+  };
+
+  return <RemoveAccountContent data={data} onConfirm={onConfirm} />;
+}
