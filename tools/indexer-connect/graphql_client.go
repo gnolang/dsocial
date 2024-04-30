8e06ad50c02324455c5b43c3bdfb66b48c09b1c2
Jeff Thompson <jeff@thefirst.org>
2024-04-30 15:38:04 +0200

    chore: Update graphql getTransactions to get all MsgCall. Use it to populate the home feed in gUserPostsByAddress. (Still need to subscribe to new MsgCall.)
    Signed-off-by: Jeff Thompson <jeff@thefirst.org>

--- tools/indexer-connect/graphql_client.go
+++ tools/indexer-connect/graphql_client.go
@@ -2,22 +2,110 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/http"
 	"net/url"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/Khan/genqlient/graphql"
 	"github.com/gorilla/websocket"
 )
 
+var gUserPostsByAddress = make(map[string]*UserPosts) // user's std.Address -> *UserPosts
+
+type UserAndPostID struct {
+	UserPostAddr string
+	PostID       int
+}
+
+type UserPosts struct {
+	homePosts []*UserAndPostID // Includes this user's threads posts plus posts of users being followed.
+	followers map[string]int   // std.Address -> startedPostsCtr of the follower
+}
+
+func newUserPosts() *UserPosts {
+	return &UserPosts{
+		homePosts: []*UserAndPostID{},
+		followers: make(map[string]int),
+	}
+}
+
+func getOrCreateUserPosts(userAddr string) *UserPosts {
+	userPosts, ok := gUserPostsByAddress[userAddr]
+	if ok {
+		return userPosts
+	}
+
+	userPosts = newUserPosts()
+	gUserPostsByAddress[userAddr] = userPosts
+	return userPosts
+}
+
+func getPostID(response string) (int, error) {
+	const postIDSuffix = " gno.land/r/berty/social.PostID)"
+	if !strings.HasSuffix(response, postIDSuffix) {
+		return 0, errors.New("Expected PostID suffix: " + response)
+	}
+	postID, err := strconv.Atoi(response[1 : len(response)-len(postIDSuffix)])
+	if err != nil {
+		return 0, err
+	}
+	return postID, nil
+}
+
 func (s *indexerService) createGraphQLClient() error {
 	s.graphQLClient = graphql.NewClient(s.remoteAddr, http.DefaultClient)
-	resp, err := getTransactions(s.ctx, s.graphQLClient, "g1jg8mtutu9khhfwc4nxmuhcpftf0pajdhfvsqf5")
+	resp, err := getTransactions(s.ctx, s.graphQLClient)
 	if err != nil {
 		return err
 	}
-	fmt.Println(resp)
+	for _, t := range resp.Transactions {
+		for _, m := range t.Messages {
+			msgCall, ok := m.Value.(*getTransactionsTransactionsTransactionMessagesTransactionMessageValueMsgCall)
+			if !ok {
+				continue
+			}
+
+			if msgCall.Func == "Follow" {
+				followedUserPosts := getOrCreateUserPosts(msgCall.Args[0])
+				startedPostsCtr, err := getPostID(t.Response.Data)
+				if err != nil {
+					fmt.Printf("Error getting PostID: %s\n", err.Error())
+					continue
+				}
+				if startedPostsCtr == 0 {
+					// Not a new following.
+					continue
+				}
+
+				followedUserPosts.followers[msgCall.Caller] = startedPostsCtr
+			}
+
+			if msgCall.Func == "PostMessage" {
+				userPosts := getOrCreateUserPosts(msgCall.Caller)
+				postID, err := getPostID(t.Response.Data)
+				if err != nil {
+					fmt.Printf("Error getting PostID: %s\n", err.Error())
+					continue
+				}
+				userAndPostID := &UserAndPostID{
+					UserPostAddr: msgCall.Caller,
+					PostID:       postID,
+				}
+				userPosts.homePosts = append(userPosts.homePosts, userAndPostID)
+
+				for followerAddr, startedPostsCtr := range userPosts.followers {
+					if postID > startedPostsCtr {
+						followerUserPosts := getOrCreateUserPosts(followerAddr)
+						followerUserPosts.homePosts = append(followerUserPosts.homePosts, userAndPostID)
+					}
+				}
+			}
+		}
+	}
 
 	clientAddr, err := websocketURL(s.remoteAddr)
 	if err != nil {
