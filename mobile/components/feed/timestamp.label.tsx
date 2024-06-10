493626677cce997418a685eac3b80db57b2dc5db
Iuri Pereira <689440+iuricmp@users.noreply.github.com>
2024-06-10 16:18:47 +0100

    feat: post date formatt
    Signed-off-by: Iuri Pereira <689440+iuricmp@users.noreply.github.com>

--- mobile/components/feed/timestamp.label.tsx
+++ mobile/components/feed/timestamp.label.tsx
@@ -1,21 +1,11 @@
+import { formatDistanceToNow } from "date-fns";
 import { colors } from "@gno/styles/colors";
 import Text from "../text";
 
 function TimeStampLabel({ timestamp }: { timestamp: string }) {
-  const date = new Date(timestamp);
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const day = date.getDate();
-  const month = date.getMonth() + 1;
-  const year = date.getFullYear();
-  const formattedHours = hours < 10 ? `0${hours}` : hours;
-  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
-  const formattedDay = day < 10 ? `0${day}` : day;
-  const formattedMonth = month < 10 ? `0${month}` : month;
-
   return (
     <Text.Caption1 style={{ color: colors.text.secondary }}>
-      {`${formattedHours}:${formattedMinutes} ${formattedDay}/${formattedMonth}/${year}`}
+      {formatDistanceToNow(new Date(timestamp), { addSuffix: false })}
     </Text.Caption1>
   );
 }
