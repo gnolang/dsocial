6482e8f8d4879d6a9d2a757d817744ca6c5d0c6f
Iuri Pereira <689440+iuricmp@users.noreply.github.com>
2024-07-02 10:18:03 +0100

    chore: case sensitive
    Signed-off-by: Iuri Pereira <689440+iuricmp@users.noreply.github.com>

--- mobile/components/feed/post-content-label.tsx
+++ mobile/components/feed/post-content-label.tsx
@@ -2,7 +2,7 @@ import { colors } from "@gno/styles/colors";
 import React from "react";
 import Autolink, { CustomMatcher } from "react-native-autolink";
 
-const PATTERN = /@([a-z]+[_a-z0-9]{5,})/gi;
+const PATTERN = /@([a-z]+[_a-z0-9]{5,})/g;
 
 type Props = {
   onMentionPress?: (accountName: string) => void;
