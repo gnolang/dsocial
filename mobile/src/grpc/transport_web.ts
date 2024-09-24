8d9e559275424d14d4ea591db3358eea25be3aac
Jeff Thompson <jeff@thefirst.org>
2024-09-24 12:34:52 +0200

    chore: In transport_web, ignore ts:check errors for declaration file in imported modules
    Signed-off-by: Jeff Thompson <jeff@thefirst.org>

--- mobile/src/grpc/transport_web.ts
+++ mobile/src/grpc/transport_web.ts
@@ -1,5 +1,7 @@
+// @ts-ignore
 import { polyfill as polyfillReadableStream } from "react-native-polyfill-globals/src/readable-stream";
 polyfillReadableStream();
+// @ts-ignore
 import { fetch as fetchPolyfill, Headers as HeadersPolyfill } from "react-native-fetch-api";
 
 import type { AnyMessage, MethodInfo, PartialMessage, ServiceType } from "@bufbuild/protobuf";
